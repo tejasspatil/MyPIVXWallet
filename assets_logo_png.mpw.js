a51684419b085544b7535cc5fc86c53de8a101fc
JSKitty <jskitty@protonmail.com>
2023-01-12 03:23:06 +0000

    Updates

---
+++ assets_logo_png.mpw.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkMPW"] = self["webpackChunkMPW"] || []).push([["assets_logo_png"],{
+
+/***/ "./assets/logo.png":
+/*!*************************!*\
+  !*** ./assets/logo.png ***!
+  \*************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "4eac9388dde139d9cef8.png";
+
+/***/ })
+
+}]);
---
+++ assets_music_mp3.mpw.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkMPW"] = self["webpackChunkMPW"] || []).push([["assets_music_mp3"],{
+
+/***/ "./assets/music.mp3":
+/*!**************************!*\
+  !*** ./assets/music.mp3 ***!
+  \**************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "8cfe53553b30cd53b4cb.mp3";
+
+/***/ })
+
+}]);

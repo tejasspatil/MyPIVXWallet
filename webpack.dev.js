337f3d756c10576cb203fc57c9ab45309c87b3d8
JSKitty <jskitty@protonmail.com>
2023-03-13 11:59:56 +0000

    Performance and SEO optimisations (#96)
    * Switch webpack to Production + Add minimizers
    
    * Import only used lodash functions
    This removes the large amount of unused lodash bloat compiled in to MPW.
    
    * Switch nav links to span
    This prevents search engines trying to index them as "href" URLs, and thus will help with solidifying indexing better.
    
    * Switch to lodash-es
    
    * Include bn.js only once
    
    * Add Dev/Prod build configurations

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devServer: {
+        static: {
+          directory: path.join(__dirname, './'),
+        },
+        compress: true,
+        port: 5500,
+        hot: true,
+        client: {
+            overlay: false
+        }
+    },
+});

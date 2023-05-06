7b8120bb70881b973112d1262e07673109b864c2
JSKitty <jskitty@protonmail.com>
2023-05-06 17:19:02 +0100

    Fix errors on clicks when MPW isn't fully loaded

--- scripts/global.js
+++ scripts/global.js
@@ -38,6 +38,14 @@ import { Address6 } from 'ip-address';
 import { getEventEmitter } from './event_bus.js';
 import { scanQRCode } from './scanner.js';
 
+/** A flag showing if base MPW is fully loaded or not */
+export let fIsLoaded = false;
+
+/** A getter for the flag showing if base MPW is fully loaded or not */
+export function isLoaded() {
+    return fIsLoaded;
+}
+
 export let doms = {};
 
 export function start() {
@@ -272,6 +280,9 @@ export function start() {
     // If allowed by settings: submit a simple 'hit' (app load) to Labs Analytics
     getNetwork().submitAnalytics('hit');
     setInterval(refreshChainData, 15000);
+
+    // After reaching here; we know MPW's base is fully loaded!
+    fIsLoaded = true;
 }
 
 function subscribeToNetworkEvents() {
@@ -318,7 +329,15 @@ let exportHidden = false;
 //                        PIVX Labs' Cold Pool
 export let cachedColdStakeAddr = 'SdgQDpS8jDRJDX8yK8m9KnTMarsE84zdsy';
 
+/**
+ * Open a UI 'tab' menu, and close all other tabs, intended for frontend use
+ * @param {Event} evt - The click event target
+ * @param {string} tabName - The name of the tab to load
+ */
 export function openTab(evt, tabName) {
+    // Only allow switching tabs if MPw is loaded
+    if (!isLoaded()) return;
+
     // Hide all screens and deactivate link highlights
     for (const domScreen of doms.arrDomScreens)
         domScreen.style.display = 'none';
